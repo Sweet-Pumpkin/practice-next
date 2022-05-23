33ac042ab50fe73233b78fa29baa81fba09afbb1
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-05-23 15:32:30 +0900

    feat: Megabyte School Next.js 학습 20220523-01

---
+++ pages/Launches.jsx
@@ -0,0 +1,36 @@
+// import React, { useState, useEffect } from 'react'
+
+export default function Launches({ data }) {
+  // const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchLanches = async () => {
+  //     const res = await fetch("https://api.spacexdata.com/v3/launches");
+  //     const data = await res.json();
+  //     setData(data);
+  //   }
+  //   fetchLanches();
+  // }, [])
+
+  if (data == null) {
+    return null;
+  }
+
+  return (
+    <div>
+      <ol>
+        { data.map((launch, idx) => (
+          <li key={ idx }>{ launch.mission_name }</li>
+        )) }
+      </ol>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://api.spacexdata.com/v3/launches");
+  const data = await res.json();
+  return {
+    props: { data },
+  }
+}
