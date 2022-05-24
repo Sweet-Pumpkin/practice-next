d825d772079b0464cce03c07b5b2ea976266a75f
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-05-24 19:22:32 +0900

    feat: Megabyte School Next.js 학습 20220524-02

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/layout';
+
+export default function FristPost() {
+  return (
+    <Layout>
+      <div>HelloWorld!</div>
+    </Layout>
+  )
+}
